60e5628046fe3eac2e3a5c453c5079e681d7c783
Guarianu <irfansban69@gmail.com>
2025-03-10 13:13:34 +0800

    update index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMPADU POLIBAN</title>
+</head>
+<body>
+    <h1>DATA MAHASISWA</h1>
+    <br>
+    <table border="1" cellspacing="0" cellspacing="5">
+        <thead>
+            <th>no</th>
+            <th>nim</th>
+            <th>nama</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>E020435</td>
+                <td>maul</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E023974</td>
+                <td>ujang</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
